dc75f0561e0a480062e8b7d8a0b63176fe61fba5
Ivan Tatur <itatur@senticode.com>
2020-11-09 00:25:47 +0300

    Added develop branch.

--- Products.API/Startup.cs
+++ Products.API/Startup.cs
@@ -33,7 +33,7 @@ namespace Products.API
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1 Develop branch");
                 c.RoutePrefix = string.Empty;
             });
 
