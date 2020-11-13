75d88e83be85baa09c40ca1962cca30524072ae0
Ivan Tatur <ivan.tatur@thomsonreuters.com>
2020-11-13 10:53:49 +0300

    Changed version

--- Products.API/Startup.cs
+++ Products.API/Startup.cs
@@ -33,7 +33,7 @@ namespace Products.API
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V2");
                 c.RoutePrefix = string.Empty;
             });
 
